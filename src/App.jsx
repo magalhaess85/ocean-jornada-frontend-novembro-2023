780514bce628ddf448a72cef8b0c60a4242f1333
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-13 19:19:50 -0400

    Desafio CSS corrigido.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ function App() {
     <>
       <div className = "card">
         <h2>Rick Sanchez</h2>
-        <h3>Status: Vivo</h3>
-        <h3>Espécie: Humana</h3>
-        <h3>Origem: Terra C-137</h3>
         <img src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
