e8d7020118ae6ef8058e27db0f51bb19f58bcf37
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-13 21:27:56 -0400

    Implementação do map para carregamento dos '<Card />'

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,18 @@ function App() {
     imageUrl:"https://rickandmortyapi.com/api/character/avatar/2.jpeg"
   }
 
+  const item3 = {
+    name: "Summer Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+  }
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <Card item = {item1} />
-      <Card item = {item2} />
+      {itens.map(function(element){
+        return <Card item = {element} />
+      })}
     </>
   )
 }
