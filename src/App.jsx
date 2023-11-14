23b0fa9fad9b32e7d0f0002a3692d0ceb0164f6b
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-14 19:59:42 -0400

    Implementação da API para carregamento de dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,53 @@
+import { useEffect, useState } from 'react';
 import './App.css'
-import Card from './components/Card/Card'
+import Card from './components/Card/Card';
 
 function App() {
   const item1 = {
     name: "Rick Sanchez",
-    imageUrl:"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
     tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
   }
 
   const item2 = {
     name: "Morty Smith",
-    imageUrl:"https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
     tags: ["Origem: Terra C-137"]
   }
 
   const item3 = {
     name: "Summer Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-    tags: ["Origem: Terra C-137"]
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
   }
 
-  const itens = [item1, item2, item3]
+  // const items = [item1, item2, item3]
 
-  const apiUrl = "https://rickandmortyapi.com/api/character/"
+  // useState
+  const [items, setItems] = useState([])
 
-  fetch(apiUrl)
+  async function carregarDadosApi() {
+    const apiUrl = "https://rickandmortyapi.com/api/character/"
+
+    const response = await fetch(apiUrl)
+
+    const body = await response.json()
+
+    setItems(body.results)
+  }
+
+  useEffect(function () {
+    carregarDadosApi()
+  }, [])
 
   return (
     <>
-      <div className = "cards">
-        {itens.map(function(element){
-          return <Card item = {element} />
+      <div className="cards">
+        {items.map(function (element) {
+          return <Card item={element} />
         })}
-        </div>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,17 @@
 import Tag from "../Tag/Tag"
 
-export default function Card(props){
-    const item = props.item
+export default function Card(props) {
+  const item = props.item
 
-    const tags = item.tags
+  const tags = item.tags || []
 
-    return <div className = "card">
+  return <div className="card">
     <h2>{item.name}</h2>
     <div className="tags">
       {tags.map(function (element) {
-        return <Tag text = {element} />
+        return <Tag text={element} />
       })}
     </div>
-    <img src = {item.imageUrl} />
+    <img src={item.image} />
   </div>
 }
