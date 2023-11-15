4278a0ba046360cf7f646598958599e7fffa1664
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-14 21:32:12 -0400

    Obtendo parâmetro de rota e adicionando ToDo's

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,3 +1,13 @@
+import { useParams } from "react-router-dom"
+
 export default function ReadById(){
-    return <div>ReadById</div>
+    const {id} = useParams()
+
+    const apiUrl =  `https://rickandmortyapi.com/api/character/${id}`
+
+    // TODO: Implementar o carregamento da API
+    // TODO: Guardar a informação recebida da API em um estado
+    // TODO: Exibir essas informções no JSX
+
+    return <div>ReadById : {id}</div>
 }
