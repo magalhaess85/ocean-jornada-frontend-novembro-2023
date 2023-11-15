5a2c88f96fe700f2b50efb53fa9746b5407395c9
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-14 21:14:31 -0400

    Criação da página `<ReadByID />'`

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,15 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Card from './components/Card/Card';
+import Home from './components/Home/Home'
+import ReadById from './components/ReadById/ReadById'
 
-function App() {
-  const [items, setItems] = useState([])
-
-  async function carregarDadosApi() {
-    const apiUrl = "https://rickandmortyapi.com/api/character/"
-
-    const response = await fetch(apiUrl)
-
-    const body = await response.json()
-
-    const results = body.results.map(function(element){
-      return{
-        name: element.name,
-        image: element.image,
-        tags: [
-          `Status: ${element.status}`,
-          `Species: ${element.species}`,
-          `Origin: ${element.origin.name}`,
-          `Episodes: ${element.episode.length}`
-        ]
-      }
-    })
-
-    setItems(results)
-  }
-
-  useEffect(function () {
-    carregarDadosApi()
-  }, [])
-
-  return (
-    <>
-      <div className="cards">
-        {items.map(function (element) {
-          return <Card item={element} />
-        })}
-      </div>
-    </>
+export default function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/:id" element = {<ReadById />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App;
