0008e08da1dff741f3e84e591d5530dd336bdeec
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-17 15:27:27 -0400

    Estilização do `<ReadById />`

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import "./ReadById.css"
+
+
 
 export default function ReadById(){
     const {id} = useParams()
@@ -14,10 +17,6 @@ export default function ReadById(){
         const body = await response.json()
 
         setItem(body)
-
-        console.log(body)
-    // TODO: Implementar o carregamento da API
-    // TODO: Guardar a informação recebida da API em um estado
     }
 
     useEffect(function(){
@@ -25,5 +24,39 @@ export default function ReadById(){
     }, [])
     
 
-    return <div>ReadById : {id} - {item.name}</div>
+    return (
+        <div className="main">
+      <div className="image-wrapper">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Imagem do Personagem" />
+      </div>
+
+      <div className="info">
+        <div className="info-tag">Status: Alive</div>
+
+        <div className="info-title">Rick Sanchez</div>
+
+        <div className="info-row">
+          <div className="info-label">Species:</div>
+          <div className="info-text">Human</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Gender:</div>
+          <div className="info-text">Male</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Origin:</div>
+          <div className="info-text">Earth (C-137)</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Location:</div>
+          <div className="info-text">Citadel of Ricks</div>
+        </div>
+      </div>
+    </div>
+
+    )
+        
 }
