82e8d9faeda0f25340d30ea17063a8c2383d7651
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-17 15:29:33 -0400

    Exibindo dados de cada personagem

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,62 +1,65 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import "./ReadById.css"
+export default function ReadById() {
+  const { id } = useParams()
+  const [item, setItem] = useState({})
+  async function carregarDadosApi() {
+    const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
+    const response = await fetch(apiUrl)
+    const body = await response.json()
+    setItem(body)
+  }
+  useEffect(function () {
+    carregarDadosApi()
+  }, [])
 
+  // TODO: Exibir essas informações no JSX
 
+  // return <div>ReadById: {id} - {item.name}</div>
 
-export default function ReadById(){
-    const {id} = useParams()
-
-    const [item, setItem] = useState({})
-
-    async function carregarDadosApi(){
-        const apiUrl =  `https://rickandmortyapi.com/api/character/${id}`
-
-        const response = await fetch(apiUrl)
-
-        const body = await response.json()
-
-        setItem(body)
-    }
-
-    useEffect(function(){
-        carregarDadosApi()
-    }, [])
-    
-
-    return (
-        <div className="main">
+  return (
+    <div className="main">
       <div className="image-wrapper">
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Imagem do Personagem" />
+        <img src={item.image} alt="Imagem do Personagem" />
       </div>
 
       <div className="info">
-        <div className="info-tag">Status: Alive</div>
+        <div className="info-tag">
+          Status: Alive
+          Status: {item.status}
+        </div>
 
-        <div className="info-title">Rick Sanchez</div>
+        <h1 className="info-title">
+          Rick Sanchez
+          {item.name}
+        </h1>
 
         <div className="info-row">
           <div className="info-label">Species:</div>
           <div className="info-text">Human</div>
+          <div className="info-text">{item.species}</div>
         </div>
 
         <div className="info-row">
           <div className="info-label">Gender:</div>
           <div className="info-text">Male</div>
+          <div className="info-text">{item.gender}</div>
         </div>
 
         <div className="info-row">
           <div className="info-label">Origin:</div>
           <div className="info-text">Earth (C-137)</div>
+          <div className="info-text">{item.origin?.name}</div>
         </div>
 
         <div className="info-row">
           <div className="info-label">Location:</div>
           <div className="info-text">Citadel of Ricks</div>
+          <div className="info-text">{item.location?.name}</div>
         </div>
       </div>
     </div>
-
     )
-        
-}
+  }
